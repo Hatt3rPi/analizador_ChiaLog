9ace9c83aa5146c8b3402f0495cdca9ba9775bcb
Hatt3rPi <fabarca212@gmail.com>
2021-07-04 21:10:39 -0400

    test parametros

--- parametros.py
+++ parametros.py
@@ -2,23 +2,35 @@ import telebot
 import pathlib
 import os
 
+
+
 ## Ingresar paratmetros ##
 # --- Telegram ---
-token_bot=""
-chat_id=""
-
+token_bot="1842675017:AAEOrbcCcdjUjA2RHJIkdExDOcYWip9mx24"
+chat_id="1567062024"
+#chat_id="-599355307" 1567062024
 
 # --- ubicación carpetas farmeo ---
-path = [""]
-blockchain={'Chia':{'', ''}, 
-'Signum':{''}, 
-'Temporales':{''}}
+path = ["C:\CHIA","E:\Plots"]
+blockchain={'Chia':{'C:\\CHIA\\', 'E:\\Plots\\'}, 
+'Signum':{'E:\\Plots Signum\\'}, 
+'Temporales':{'E:\\Plotter', 'F:\\Plots','D:\\Plots', 'C:\\Plots'}}
+
+
+
 
 aviso_telegram=True
 check_plot=True
+# -- obsoletos---
+"""min_busq=1
+aviso_plots_nuevos=True
+aviso_diario=True
+aviso_diario_hora=[23,00]
+"""
 check_plot_nro_proof=50
 
 #---- Funcionamiento ----
+
 
 version_chia="app-1.1.7"
 ruta_daemon=f"C:\\Users\\{os.getlogin()}\\AppData\Local\chia-blockchain\\{version_chia}\\resources\\app.asar.unpacked\\daemon"  
